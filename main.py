68491fb4ce4a28f1e89cf4d987fa9caf7d287b38
Kevin Ahr <meowmeowahr@gmail.com>
2024-08-05 09:50:52 -0500

    Cleanup

--- main.py
+++ main.py
@@ -62,17 +62,6 @@ class Motions(utils.ExtendedIntEnum):
     MANUAL = 3
 
 
-class MotionSegment(enum.Enum):
-    """
-    Segments of animation
-    """
-
-    CENTER_FROM_LEFT = 0
-    CENTER_FROM_RIGHT = 1
-    LEFT = 2
-    RIGHT = 3
-
-
 class RobotEyes:
     def __init__(self):
         self.settings = {}
@@ -93,9 +82,6 @@ class RobotEyes:
         self.state = State.LOGO
         self.motion = Motions(self.settings["states"]["motion"])
 
-        self.eye_x = 120
-        self.eye_y = 120
-
         # Init display
         self.display_1_cs = digitalio.DigitalInOut(board.CE0)
         self.display_1_dc = digitalio.DigitalInOut(board.D25)
@@ -105,14 +91,15 @@ class RobotEyes:
         self.display_2_dc = digitalio.DigitalInOut(board.D23)
         self.display_2_rt = digitalio.DigitalInOut(board.D22)
 
-        self.bl_pin = 16
-
         self.spi_0 = busio.SPI(clock=board.SCK, MOSI=board.MOSI)
         self.spi_1 = busio.SPI(clock=board.SCK_1, MOSI=board.MOSI_1)
 
+        # Init display backlight
+        self.bl_pin = 16
         self.backlight = PWMLED(self.bl_pin)
         self.backlight.value = self.settings["display"]["backlight"] / 100
 
+        # Init ST7789 displays
         self.display_0 = st7789.ST7789(
             self.spi_0,
             height=240,
@@ -143,7 +130,14 @@ class RobotEyes:
             self.width = self.display_0.width
             self.height = self.display_0.height
 
+        # Set initial eye position
+        self.eye_x, self.eye_y = self.settings["motions"]["center_point"]
+
     def run(self):
+        """
+        Run eyes in a loop
+        Start threads for serial and motion
+        """
         serial_thread = threading.Thread(target=self.serial_loop, daemon=True)
         serial_thread.start()
 
@@ -171,14 +165,18 @@ class RobotEyes:
         """
         Show loading page while main system connects
         """
+
+        # Create image
         image = Image.new("RGB", (self.width, self.height))
         draw = ImageDraw.Draw(image)
 
+        # Fill background
         draw.rectangle(
             (0, 0, self.width, self.height),
             fill=self.settings["loading_format"]["color"],
         )
 
+        # Border
         draw.rectangle(
             (
                 self.settings["loading_format"]["border"],
@@ -189,6 +187,7 @@ class RobotEyes:
             fill=self.settings["loading_format"]["bg_color"],
         )
 
+        # Init font
         font = ImageFont.truetype(
             self.settings["loading_format"]["font"],
             self.settings["loading_format"]["font_size"],
@@ -197,6 +196,7 @@ class RobotEyes:
             self.settings["loading_format"]["text"]
         )
 
+        # Add text
         draw.text(
             (self.width // 2 - font_width // 2, self.height // 2 - font_height // 2),
             self.settings["loading_format"]["text"].format("error"),
@@ -204,63 +204,57 @@ class RobotEyes:
             fill=self.settings["loading_format"]["color"],
         )
 
+        # Display image
         self.display_0.image(image)
         self.display_1.image(image)
 
     def error_periodic(self, error=0):
-        global last_redraw, error_border_visible
+        # Create image
+        image = Image.new("RGB", (self.width, self.height))
+        draw = ImageDraw.Draw(image)
 
-        if last_redraw + self.settings["error_format"]["flash_speed"] < time.time():
-            image = Image.new("RGB", (self.width, self.height))
-            draw = ImageDraw.Draw(image)
+        # Fill background
+        draw.rectangle(
+            (0, 0, self.width, self.height),
+            fill=self.settings["error_format"]["color"],
+        )
 
-            error_border_visible = not error_border_visible
-            if error_border_visible:
-                draw.rectangle(
-                    (0, 0, self.width, self.height),
-                    fill=self.settings["error_format"]["color"],
-                )
-            else:
-                draw.rectangle(
-                    (0, 0, self.width, self.height),
-                    fill=self.settings["error_format"]["bg_color"],
-                )
+        # Border
+        draw.rectangle(
+            (
+                self.settings["error_format"]["border"],
+                self.settings["error_format"]["border"],
+                self.width - self.settings["error_format"]["border"] - 1,
+                self.height - self.settings["error_format"]["border"] - 1,
+            ),
+            fill=self.settings["error_format"]["bg_color"],
+        )
 
-            draw.rectangle(
-                (
-                    self.settings["error_format"]["border"],
-                    self.settings["error_format"]["border"],
-                    self.width - self.settings["error_format"]["border"] - 1,
-                    self.height - self.settings["error_format"]["border"] - 1,
-                ),
-                fill=self.settings["error_format"]["bg_color"],
-            )
+        # Init font
+        font = ImageFont.truetype(
+            self.settings["error_format"]["font"],
+            self.settings["error_format"]["font_size"],
+        )
+        (_, _, font_width, font_height) = font.getbbox(
+            self.settings["error_format"]["text"].format(error)
+        )
 
-            font = ImageFont.truetype(
-                self.settings["error_format"]["font"],
-                self.settings["error_format"]["font_size"],
-            )
-            (_, _, font_width, font_height) = font.getbbox(
-                self.settings["error_format"]["text"].format(error)
-            )
+        # Add text
+        draw.text(
+            (
+                self.width // 2 - font_width // 2,
+                self.height // 2 - font_height // 2,
+            ),
+            self.settings["error_format"]["text"].format(error),
+            font=font,
+            fill=self.settings["error_format"]["color"],
+        )
 
-            draw.text(
-                (
-                    self.width // 2 - font_width // 2,
-                    self.height // 2 - font_height // 2,
-                ),
-                self.settings["error_format"]["text"].format(error),
-                font=font,
-                fill=self.settings["error_format"]["color"],
-            )
-
-            self.display_0.image(image)
-            self.display_1.image(image)
-            last_redraw = time.time()
+        # Display image
+        self.display_0.image(image)
+        self.display_1.image(image)
 
     def tv_static_periodic(self):
-        global last_redraw
-
         if self.last_redraw + 0.1 < time.time():
             image = Image.new("RGB", (self.width, self.height))
 
@@ -279,13 +273,20 @@ class RobotEyes:
 
             self.display_0.image(image)
             self.display_1.image(image)
-            last_redraw = time.time()
+            self.last_redraw = time.time()
 
     def eye_motion(self):
-        motion_segment = MotionSegment.RIGHT
+        """
+        Motion loop
+        
+        Supports Smooth movement, Kevinbot v2 style jumpy motion, and manual control
+        """
         step = 0
         while True:
             if self.motion == Motions.LEFT_RIGHT:
+                """
+                Smooth movement with Cubic In Out curve
+                """
                 num_steps = utils.map_range(
                     self.settings["motions"]["speed"], 0, 100, 620, 20
                 )
@@ -306,6 +307,9 @@ class RobotEyes:
                 step += 2 / num_steps
 
             elif self.motion == Motions.JUMP:
+                """
+                Kevinbot v2 style jumpy motion
+                """
                 num_steps = utils.map_range(
                     self.settings["motions"]["speed"], 0, 100, 620, 20
                 )
@@ -326,6 +330,9 @@ class RobotEyes:
                 step += 2 / num_steps
 
             elif self.motion == Motions.MANUAL:
+                """
+                Manual control via serial commands
+                """
                 self.eye_x, self.eye_y = self.settings["motions"]["pos"]
                 time.sleep(0.01)
 
@@ -341,12 +348,14 @@ class RobotEyes:
     def main_loop(self):
         """
         Display loop
+
         Update displays with skin
         """
         start_time = time.time()
         self.request_handshake()
         last_handshake_request = time.time()
         while True:
+            # Display state
             if self.state == State.LOGO:
                 self.create_logo()
                 if time.time() - start_time > self.settings["logo_format"]["logo_time"]:
@@ -359,6 +368,7 @@ class RobotEyes:
             elif self.state == State.ERORR:
                 self.error_periodic(self.settings["states"]["error"])
             elif self.state == State.HOME:
+                # Eye skin state
                 if self.visual_page == VisualPage.STATE_TV_STATIC:
                     self.tv_static_periodic()
                 elif self.visual_page == VisualPage.STATE_EYE_SIMPLE:
@@ -388,19 +398,28 @@ class RobotEyes:
             time.sleep(0.022)  # 45fps
 
     def serial_loop(self):
+        """
+        Loop to recieve serial data
+        """
+
         # send settings on start
         settings_copy = copy.deepcopy(self.settings)
         settings_copy.pop("error_format")
         utils.send_data(settings_copy, self.ser, "eye_settings.")
 
         while True:
+            # decode data
             data = self.ser.readline().decode("UTF-8")
             pair = data.strip("\r\n").split("=", 1)
+
             print(pair)
+            # commands that don't set a value
             if pair[0] == "handshake.complete":
                 self.state = State.HOME
+            # comands that do set a value
             elif len(pair) == 2:
                 if pair[0] == "setState":
+                    # set visual page of display
                     if pair[1].isdigit():
                         self.settings["states"]["page"] = utils.clamp(
                             int(pair[1]), 1, len(VisualPage.list())
@@ -409,20 +428,28 @@ class RobotEyes:
                         self.save_settings()
                         previous_time = time.time()
                 elif pair[0] == "setError":
+                    # retrieve an error code display
                     if pair[1].isdigit():
                         self.settings["states"]["error"] = int(pair[1])
                         self.save_settings()
                 elif pair[0] == "setSkinOption":
-                    option_pairs = pair[1].split(":")
+                    # set a skin option
+                    option_pairs = pair[1].split(":") # ex: ["simple", "bg_color", "#000000"]
+
+                    # require there to be 3 values (skin name, option, and value)
                     if len(option_pairs) == 3:
+                        # if the value consists of only digits, convert it to an int
                         if option_pairs[2].isdigit():
                             value = int(option_pairs[2])
                         else:
                             value = option_pairs[2]
+
+                        # check if the skin name is valid
                         if not option_pairs[0] in self.settings["skins"]:
                             logging.warning("Skin %s does not exist", option_pairs[0])
                             continue
 
+                        # check if the option is valid
                         if (
                             not option_pairs[1]
                             in self.settings["skins"][option_pairs[0]]
@@ -434,6 +461,8 @@ class RobotEyes:
                             )
                             continue
 
+                        # ensure that the option is not a list or tuple, they can't be changed as of now
+                        # TODO: Make list and tuple options compatible
                         if type(
                             self.settings["skins"][option_pairs[0]][option_pairs[1]]
                         ) in (
@@ -449,7 +478,9 @@ class RobotEyes:
                     else:
                         logging.warning("Expected 3 values, got %s", len(option_pairs))
                 elif pair[0] == "setMotion":
+                    # set the active motion mode
                     if pair[1].isdigit():
+                        # check if the requested value is valid
                         if int(pair[1]) in range(len(Motions.list())):
                             self.settings["states"]["motion"] = int(pair[1])
                             self.motion = Motions(int(pair[1]))
@@ -457,10 +488,12 @@ class RobotEyes:
                     else:
                         logging.warning("Expected digits, got %s", pair[1])
                 elif pair[0] == "getSettings":
+                    # send all settings over serial
                     settings_copy = copy.deepcopy(self.settings)
                     settings_copy.pop("error_format")
                     utils.send_data(settings_copy, self.ser, "eyeSettings.")
                 elif pair[0] == "setBacklight":
+                    # set backlight brightness
                     if pair[1].isdigit():
                         self.backlight.value = int(pair[1]) / 100
                         self.settings["display"]["backlight"] = int(pair[1])
